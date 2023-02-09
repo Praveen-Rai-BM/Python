7a723c70dbe2dc82efd480e34b323bf4c2301d92
Praveen-Rai-BM <123984444+Praveen-Rai-BM@users.noreply.github.com>
2023-02-10 00:04:16 +0530

    Data Types and Input

---
+++ Data Types.py
@@ -0,0 +1,31 @@
+#Data Type
+# int :store whole number eg.age=22
+# flot  :stores numbers with deceimal point. eg. pi=3.4
+# boolean  :True,False (1,0)
+# Str :group of Characters
+# Complex  :have a real part an imaginary eg.3+4j
+
+
+age=22
+print("datatype of age: ",type(age))
+
+pi=3.4
+print("datatype of pi: ",type(pi))
+
+st_pass=True
+print("dtatype of st_pas:",type(st_pass))
+
+name="Praveen"
+print("datatype of name:",type(name))
+
+C_num=6+6j
+print("datatype of C_num:",type(C_num))
+
+
+#--------------------
+# bool -> True, False -> 0,1
+#False Values in python: False,0,None,"",(),[],{}
+print(bool(0))
+print(bool(""))
+print(bool("1"))
+print(bool(-90))
---
+++ Input.py
@@ -0,0 +1,12 @@
+# getting input from the user
+age =int(input("Enter your age: "))
+print("Vlue of age is: ",age)
+
+
+
+new_age= age+5
+#print("Your new age is: ",new_age)
+
+#print("Datatype of age: ",type(age)
+
+print("your new age is: ",new_age)
